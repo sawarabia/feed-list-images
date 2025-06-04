866fb2bf25cefa57ce5f9a17095bbe86cd1a62af
sawarabia <sw1911zhanchi@gmail.com>
2025-06-04 15:11:39 +0900

    負荷軽減テスト

--- src/server.ts
+++ src/server.ts
@@ -63,7 +63,8 @@ export class FeedGenerator {
 
   async start(): Promise<http.Server> {
     await migrateToLatest(this.db)
-    this.firehose.run(this.cfg.subscriptionReconnectDelay)
+    // 高負荷対策のため止める
+    // this.firehose.run(this.cfg.subscriptionReconnectDelay)
     this.server = this.app.listen(this.cfg.port, this.cfg.listenhost)
     await events.once(this.server, 'listening')
     return this.server
