6ff9f91bbde3e33e2dd476394be8653568e145e0
sawarabia <sw1911zhanchi@gmail.com>
2025-06-06 11:44:56 +0900

    DB移行対応

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,22 @@
 import SqliteDb from 'better-sqlite3'
-import { Kysely, Migrator, SqliteDialect } from 'kysely'
+import { Kysely, Migrator, SqliteDialect, PostgresDialect } from 'kysely'
+import { Pool } from 'pg'
 import { DatabaseSchema } from './schema'
 import { migrationProvider } from './migrations'
 
 export const createDb = (location: string): Database => {
-  return new Kysely<DatabaseSchema>({
-    dialect: new SqliteDialect({
-      database: new SqliteDb(location),
-    }),
-  })
+  if (location.startsWith('postgres://')) {
+    const pool = new Pool({ connectionString: location })
+    return new Kysely<DatabaseSchema>({
+      dialect: new PostgresDialect({ pool }),
+    })
+  } else {
+    return new Kysely<DatabaseSchema>({
+      dialect: new SqliteDialect({
+        database: new SqliteDb(location),
+      }),
+    })
+  }
 }
 
 export const migrateToLatest = async (db: Database) => {
