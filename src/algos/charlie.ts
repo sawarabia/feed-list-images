957bff6f7d72cb74a76e5df64d064c54dcc69712
sawarabia <sw1911zhanchi@gmail.com>
2025-06-21 11:42:49 +0900

    コピペミス修正

--- src/algos/charlie.ts
+++ src/algos/charlie.ts
@@ -3,7 +3,7 @@ import { QueryParams } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import { AppContext } from '../config'
 
 // max 15 chars
-export const shortname = 'india'
+export const shortname = 'charlie'
 
 export const handler = async (ctx: AppContext, params: QueryParams) => {
   const listUri =
