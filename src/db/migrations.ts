07b8863ac03d1dc7cb87e701f4519f34476fafc9
sawarabia <sw1911zhanchi@gmail.com>
2025-06-21 12:05:17 +0900

    DBエラー対応

--- src/db/migrations.ts
+++ src/db/migrations.ts
@@ -12,11 +12,13 @@ migrations['001'] = {
   async up(db: Kysely<unknown>) {
     await db.schema
       .createTable('post')
-      .addColumn('uri', 'varchar', (col) => col.primaryKey())
+      .addColumn('uri', 'varchar', (col) => col.notNull())
       .addColumn('cid', 'varchar', (col) => col.notNull())
       .addColumn('indexedAt', 'varchar', (col) => col.notNull())
       .addColumn('listUri', 'varchar', (col) => col.notNull())
       .addColumn('postType', 'varchar', (col) => col.notNull())
+      .addPrimaryKeyConstraint('post_pk', ['uri'])
+      .addUniqueConstraint('post_unique', ['uri', 'postType'])
       .execute()
     await db.schema
       .createTable('sub_state')
