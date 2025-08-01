fb88b56d7c6bfc9e0601cf8e112071fa6cc432af
Syuji Wakushima <syuji.wakushima.dg@ai.k-cloud85.biz>
2025-08-01 17:31:20 +0900

    デプロイエラー対応

--- src/subscription.ts
+++ src/subscription.ts
@@ -9,49 +9,49 @@ import dotenv from 'dotenv'
 import { QueryParams as QueryParamsFeeds } from './lexicon/types/app/bsky/feed/getAuthorFeed'
 
 // このクラスは使わない
-export class FirehoseSubscription extends FirehoseSubscriptionBase {
-  async handleEvent(evt: RepoEvent) {
-    if (!isCommit(evt)) return
+// export class FirehoseSubscription extends FirehoseSubscriptionBase {
+//   async handleEvent(evt: RepoEvent) {
+//     if (!isCommit(evt)) return
 
-    const ops = await getOpsByType(evt)
+//     const ops = await getOpsByType(evt)
 
-    // This logs the text of every post off the firehose.
-    // Just for fun :)
-    // Delete before actually using
-    for (const post of ops.posts.creates) {
-      console.log(post.record.text)
-    }
+//     // This logs the text of every post off the firehose.
+//     // Just for fun :)
+//     // Delete before actually using
+//     for (const post of ops.posts.creates) {
+//       console.log(post.record.text)
+//     }
 
-    const postsToDelete = ops.posts.deletes.map((del) => del.uri)
-    const postsToCreate = ops.posts.creates
-      .filter((create) => {
-        // only alf-related posts
-        return create.record.text.toLowerCase().includes('alf')
-      })
-      .map((create) => {
-        // map alf-related posts to a db row
-        return {
-          uri: create.uri,
-          cid: create.cid,
-          indexedAt: new Date().toISOString(),
-        }
-      })
+//     const postsToDelete = ops.posts.deletes.map((del) => del.uri)
+//     const postsToCreate = ops.posts.creates
+//       .filter((create) => {
+//         // only alf-related posts
+//         return create.record.text.toLowerCase().includes('alf')
+//       })
+//       .map((create) => {
+//         // map alf-related posts to a db row
+//         return {
+//           uri: create.uri,
+//           cid: create.cid,
+//           indexedAt: new Date().toISOString(),
+//         }
+//       })
 
-    if (postsToDelete.length > 0) {
-      await this.db
-        .deleteFrom('post')
-        .where('uri', 'in', postsToDelete)
-        .execute()
-    }
-    if (postsToCreate.length > 0) {
-      await this.db
-        .insertInto('post')
-        .values(postsToCreate)
-        .onConflict((oc) => oc.doNothing())
-        .execute()
-    }
-  }
-}
+//     if (postsToDelete.length > 0) {
+//       await this.db
+//         .deleteFrom('post')
+//         .where('uri', 'in', postsToDelete)
+//         .execute()
+//     }
+//     if (postsToCreate.length > 0) {
+//       await this.db
+//         .insertInto('post')
+//         .values(postsToCreate)
+//         .onConflict((oc) => oc.doNothing())
+//         .execute()
+//     }
+//   }
+// }
 
 export class ListMembersSubscription {
   agent: AtpAgent
